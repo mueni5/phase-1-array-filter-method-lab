e155a4e4e7ff04dfdefd131cc7538ebf598485d9
+ <+>
2023-03-26 21:41:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+      return name ? name === "Susan" : [];    
+    }
+
+    function fuzzyMatch(drivers, letters) {
+        return drivers.filter(letters =>'Sa');
+        return letters ? letters=== 'mm' : [];
+    
+
+    }
+
+
+    function matchName(drivers, name) {
+        drivers = [
+          {
+            name: 'Bobby',
+            hometown: 'Pittsburgh' },
+          {
+            name: 'Sammy',
+            hometown: 'New York' } ,
+          {
+            name: 'Sally',
+            hometown: 'Cleveland' },
+          {
+            name: 'Annette',
+            hometown: 'Los Angeles' },
+          {
+            name: 'Bobby',
+            hometown: 'Tampa Bay' }
+        ];
+        return drivers.filter(driver => driver.name === "Bobby");
+      }
+
